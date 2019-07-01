25af411f85e2c5315ebabc26fa02325ff8fd55df
waylony <454836582@qq.com>
2019-07-01 14:02:42 +0800

    初始化框架

--- domain/src/main/java/com/afabao/travel/test/Test.java
+++ domain/src/main/java/com/afabao/travel/test/Test.java
@@ -1,8 +1,11 @@
 package com.afabao.travel.test;
 
+import lombok.Data;
+
 /**
  * Created by Wells on 2019/6/25 0025.
  */
+@Data
 public class Test {
     private Long id;
     private String name;
--- travel/src/main/java/com/afabao/travel/controller/TestController.java
+++ travel/src/main/java/com/afabao/travel/controller/TestController.java
@@ -3,8 +3,10 @@ package com.afabao.travel.controller;
 import com.afabao.travel.service.TestService;
 import com.afabao.travel.test.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,14 +14,16 @@ import java.util.List;
 /**
  * Created by Wells on 2019/6/25 0025.
  */
-@RestController
+@Controller
 public class TestController {
 
     @Autowired
     private TestService service;
 
     @RequestMapping(value = "/test",method = RequestMethod.GET)
+    @ResponseBody
     public List<Test> list(){
+        System.out.println("ENTER");
         return service.list();
     }
 }
